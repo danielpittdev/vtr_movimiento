b0e5c155ac1d408cc776a01edfc14bd6c0d0b949
danielpittdev <oficialdanielpitt16@gmail.com>
2025-09-15 17:04:07 +0200

    Web funcional v1.2

--- resources/views/vendor/mail/html/header.blade.php
+++ resources/views/vendor/mail/html/header.blade.php
@@ -2,11 +2,7 @@
 <tr>
    <td class="header" style="background:#333">
       <a href="{{ $url }}" style="display: inline-block;">
-         @if (trim($slot) === 'Laravel')
-            <img src="https://vtrmovimiento.com/media/logo.png" class="logo" alt="VTR">
-         @else
-            {!! $slot !!}
-         @endif
+         <img src="https://vtrmovimiento.com/media/logo.png" class="logo" alt="VTR">
       </a>
    </td>
 </tr>
