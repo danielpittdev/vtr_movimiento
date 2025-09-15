8d22ed2f018a9bd2ce4ff996677035e0c14443c8
danielpittdev <oficialdanielpitt16@gmail.com>
2025-09-15 23:24:03 +0200

    Correción por Sara

--- resources/views/invitados.blade.php
+++ resources/views/invitados.blade.php
@@ -12,7 +12,9 @@
                   class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75">
                </div>
 
-               <video src="/media/videos/back3.mp4" loop autoplay muted
+               <video src="/media/videos/back3.mp4" loop autoplay muted playsinline
+                  webkit-playsinline x5-playsinline controlsList="nofullscreen noremoteplayback" disablepictureinpicture
+                  controls
                   class="absolute scale-[4] top-0 w-full h-full"></video>
             </div>
 
