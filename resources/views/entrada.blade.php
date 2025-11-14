0b3fb42d8e5479877c8d74b2f5ae28f0637eeae8
Daniel <dgonzalezpitt@safareyes.es>
2025-11-14 21:25:23 +0100

    cccc
    cccc

--- resources/views/entrada.blade.php
+++ resources/views/entrada.blade.php
@@ -4,12 +4,6 @@
    <div class="bg-base-100">
 
       <main class="isolate h-[100vh] flex items-center justify-center bg-base-200 p-3">
-         <div aria-hidden="true" class="absolute inset-x-0 -z-10 transform-gpu overflow-hidden blur h-full">
-            <div
-               style="clip-path: polygon(74.1% 44.1%, 100% 61.6%, 97.5% 26.9%, 85.5% 0.1%, 80.7% 2%, 72.5% 32.5%, 60.2% 62.4%, 52.4% 68.1%, 47.5% 58.3%, 45.2% 34.5%, 27.5% 76.7%, 0.1% 64.9%, 17.9% 100%, 27.6% 76.8%, 76.1% 97.7%, 74.1% 44.1%)"
-               class="relative left-[calc(50%+3rem)] aspect-1155/678 w-144.5 -translate-x-1/2 bg-linear-to-tr from-[#ff80b5] to-[#9089fc] opacity-30 sm:left-[calc(50%+36rem)] sm:w-288.75">
-            </div>
-         </div>
          <div class="bg-base-100 shadow-lg rounded p-3 max-w-md w-full space-y-6 border border-base-content/20">
 
             <h1 class="font-medium">
