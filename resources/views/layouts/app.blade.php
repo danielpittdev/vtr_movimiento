e313ca3bb796c78139cb84f912de03ea7533beee
Jose Lorite <39122039+modasele@users.noreply.github.com>
2026-04-17 10:04:54 +0200

    layout limpio con OG dinámico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -3,7 +3,17 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>VTR</title>
+
+    <title>@yield('title', 'VTR - Venga Tu Reino')</title>
+
+    <meta property="og:title" content="@yield('og_title', 'VTR - Venga Tu Reino')">
+    <meta property="og:description" content="@yield('og_description', 'Soñemos con lo que Dios quiere hacer en España con nuestra generación')">
+    <meta property="og:image" content="@yield('og_image', 'https://vtrmovimiento.com/media/fotos/portada.jpg')">
+    <meta property="og:url" content="{{ url()->current() }}">
+    <meta property="og:type" content="website">
+
+    <meta name="twitter:card" content="summary_large_image">
+
     @vite(['resources/css/app.css', 'resources/js/app.js'])
 </head>
 
@@ -14,13 +24,6 @@
     <main>
         @yield('content')
     </main>
-<meta property="og:title" content="VTR - Venga Tu Reino">
-<meta property="og:description" content="Soñemos con lo que Dios quiere hacer en España con nuestra generación">
-<meta property="og:image" content="https://vtrmovimiento.com/media/fotos/portada.jpg">
-<meta property="og:url" content="https://vtrmovimiento.com">
-<meta property="og:type" content="website">
-
-<meta name="twitter:card" content="summary_large_image">
 
 </body>
 </html>
