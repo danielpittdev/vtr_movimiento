4b94596c835da214108962a7458a42d93d6a5a73
Jose Lorite <39122039+modasele@users.noreply.github.com>
2026-04-17 10:18:59 +0200

    “modificaciones”

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -9,6 +9,9 @@
     <meta property="og:title" content="@yield('og_title', 'VTR - Venga Tu Reino')">
     <meta property="og:description" content="@yield('og_description', 'Soñemos con lo que Dios quiere hacer en España con nuestra generación')">
     <meta property="og:image" content="@yield('og_image', 'https://vtrmovimiento.com/media/fotos/portada.jpg')">
+
+<meta property="og:image:width" content="1200">
+<meta property="og:image:height" content="630">
     <meta property="og:url" content="{{ url()->current() }}">
     <meta property="og:type" content="website">
 
